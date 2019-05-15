32f996015be2630e2fd4c6dbcb3e8f2dba374ed3
Quentin Brosse <quentin.brosse@icloud.com>
2019-05-15 20:14:41 +0200

    implement sdkLogger.ShouldLog

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ require (
 	github.com/hashicorp/terraform v0.12.0
 	github.com/mitchellh/go-homedir v1.0.0
 	github.com/nicolai86/scaleway-sdk v0.0.0-20181024210327-b20018e944c4
-	github.com/scaleway/scaleway-sdk-go v0.0.0-20190514163101-29930cd612eb
+	github.com/scaleway/scaleway-sdk-go v0.0.0-20190515170301-576e6e677683
 	github.com/smartystreets/goconvey v0.0.0-20190222223459-a17d461953aa // indirect
 	golang.org/x/crypto v0.0.0-20190426145343-a29dc8fdc734
 )
--- scaleway/sdk_logger.go
+++ scaleway/sdk_logger.go
@@ -1,6 +1,12 @@
 package scaleway
 
-import "log"
+import (
+	"log"
+
+	"github.com/hashicorp/terraform/helper/logging"
+
+	"github.com/scaleway/scaleway-sdk-go/logger"
+)
 
 // sdkLogger is the implementation of the SDK Logger interface for this terraform plugin.
 //
@@ -26,4 +32,9 @@ func (l sdkLogger) Warningf(format string, args ...interface{}) {
 // Errorf logs to the ERROR log. Arguments are handled in the manner of fmt.Printf.
 func (l sdkLogger) Errorf(format string, args ...interface{}) {
 	log.Printf("[EROR] "+format, args...)
+}
+
+// ShouldLog allow the SDK to log only in DEBUG or TRACE levels.
+func (l sdkLogger) ShouldLog(level logger.LogLevel) bool {
+	return logging.IsDebugOrHigher()
 }
